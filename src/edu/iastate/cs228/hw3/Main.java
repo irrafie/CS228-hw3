22fedeb69ddeeb675e50d2d588b18fce792a9848
irrafie <irrafie@iastate.edu>
2018-02-26 12:54:10 -0600

    root

---
+++ src/edu/iastate/cs228/hw3/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author  Irfan Farhan Mohamad Rafie
+ */
+
+package edu.iastate.cs228.hw3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
